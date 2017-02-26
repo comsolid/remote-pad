35d7e48f5955a620ea5d09f54158ffb71f937ed3
Átila Camurça <camurca.home@gmail.com>
2017-02-26 11:19:47 -0300

    Script para gerar versão compatível com electron
    Usada no Remote Pad GUI

---
+++ scripts/build4electron.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Generates a version for use with remote-pad-gui (electron)
+#
+
+npm run build
+if [[ -d electron ]]; then
+    echo "Removing previous build for electron..."
+    rm -r electron
+fi
+mkdir -p electron
+echo "Copying files..."
+cp -r dist production package.json yarn.lock electron
+cd electron
+yarn install --production
+cd -
+echo "Done."
