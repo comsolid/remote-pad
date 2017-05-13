4cf6899378d0bdce2e407c39377e2291559fc729
Átila Camurça <camurca.home@gmail.com>
2017-05-13 14:24:29 -0300

    Initial support for Dolphin (Game Cube) added
    TODO: Create more layouts for games like Mario Party.
    
    Fixed #46.

---
+++ src/store/laf/race/ngc--default.js
@@ -0,0 +1,72 @@
+
+import { GREEN, WHITESMOKE, WHITE, RED, SILVER } from '../../colors/ngc--default'
+
+module.exports = {
+    pad: {
+        bgColor: WHITESMOKE
+    },
+    btnA: {
+        bgColor: SILVER,
+        fgColor: WHITE,
+        label: 'X',
+        keyb: 'Y'
+    },
+    btnB: {
+        bgColor: GREEN,
+        fgColor: WHITE,
+        label: 'A',
+        keyb: 'A'
+    },
+    btnY: {
+        bgColor: RED,
+        fgColor: WHITE,
+        label: 'B',
+        keyb: 'B'
+    },
+    btnX: {
+        bgColor: SILVER,
+        fgColor: WHITE,
+        label: 'R',
+        keyb: 'X'
+    },
+    directional: {
+        bgColor: SILVER,
+        fgColor: WHITE,
+        label: 'Directional',
+        icon: 'fa-directional'
+    },
+    left: {
+        bgColor: SILVER,
+        fgColor: WHITE,
+        label: 'Left',
+        icon: 'fa-caret-left',
+        keyb: 'left'
+    },
+    right: {
+        bgColor: SILVER,
+        fgColor: WHITE,
+        label: 'Right',
+        icon: 'fa-caret-right',
+        keyb: 'right'
+    },
+    up: {
+        bgColor: SILVER,
+        fgColor: WHITE,
+        label: 'Up',
+        icon: 'fa-caret-up',
+        keyb: 'up'
+    },
+    down: {
+        bgColor: SILVER,
+        fgColor: WHITE,
+        label: 'Down',
+        icon: 'fa-caret-down',
+        keyb: 'down'
+    },
+    btnStart: {
+        bgColor: SILVER,
+        fgColor: WHITE,
+        label: 'Start',
+        keyb: 'start'
+    }
+}
